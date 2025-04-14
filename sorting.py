42b9290269b45a7c99da78af8394bc53c1364c3a
marucmiralova <marucmiralova@seznam.cz>
2025-04-14 09:19:12 +0200

    selection sort

--- sorting.py
+++ sorting.py
@@ -22,11 +22,47 @@ def read_data(file_name):
                     data[header].append(int(value))
     return data
 
+#seznam = [88, 36, 21, 54, 99, 1, 81, 18, 21, 36, 61]
+# my_list = [1,2,3,4]
+# my_list[1], my_list[3] = my_list[3], my_list[1] ## [1, 4, 3, 2]
 
+def selection_sort(number_seznam, direction = "ascending"):
+    """
+    :param list number_seznam: list witch numeric array
+    :param str direction: indicates sorting direction: ascending/descending
+    :return: sorted number_seznam
+    """
+    for i in range(len(number_seznam)):
+        idx = i
+        for j in range(i+1, len(number_seznam)):
+            if(direction == "ascending" and number_seznam[j] < number_seznam[idx]) or (direction == "descending" and number_seznam[j] > number_seznam[idx]):
+                idx = j
+        number_seznam[i], number_seznam[idx]  = number_seznam[idx], number_seznam[i]
+    return number_seznam
+
+## nefunkcni opis z tabule, ale je to totez co vyse
+# def selection_sort(nmb_array, direction = "ascending"):
+#     n = len(nmb_array)
+#     for i in range(n):
+#         min_max_i = i
+#         for num_idx in range(n+1, n):
+#             if direction == "ascending":
+#                 if nmb_array[num_idx] < nmb_array[min_max_i]:
+#                     min_max_i = num_idx
+#             elif direction == "descending":
+#                 if nmb_array[num_idx] > nmb_array[min_max_i]:
+#                     min_max_i = num_idx
+#         nmb_array[i], nmb_array[min_max_i] = nmb_array[min_max_i], nmb_array[i]
+#
+#     return nmb_array
 
 def main():
     data = read_data("numbers.csv")
     print(data)
+    seznam = data["series_1"]
+    # print(seznam) # [88, 36, 21, 54, 99, 1, 81, 18, 21, 36, 61]
+    selection = selection_sort(seznam)
+    print(selection) # [1, 18, 21, 21, 36, 36, 54, 61, 81, 88, 99]
     pass
 
 
